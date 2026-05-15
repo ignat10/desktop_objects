a1e679f23556602e909ace449d7c5f8ee3027dfb
Ignat10 <Ignat.shikh@gmail.com>
2026-05-15 19:54:08 +0200

    add basic click(x, y) func

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,28 @@
+mod screen;
+
 use pyo3::prelude::*;
+use enigo::{Settings, Enigo, Mouse, Button, Direction, Coordinate};
+
+
+
+fn mouse() -> Enigo {
+    Enigo::new(&Settings::default()).unwrap()
+}
+
 
 #[pymodule]
 mod desktop_objects {
     use super::*;
 }
+
+
+fn click(
+    x: impl Into<i32>,
+    y: impl Into<i32>
+) {
+    let mut mouse = mouse();
+    mouse.move_mouse(x.into(), y.into(), Coordinate::Abs).unwrap();
+    mouse.button(Button::Left, Direction::Click).unwrap();
+}
+
+
